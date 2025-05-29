5a6d5d32fc5b4c88e3a6de27a6205995fede0ee0
hugolnx <hugolnx@gmail.com>
2025-05-29 19:37:03 -0300

    Screen service auto-detects resolution

--- Runtime/Services/ScreenService.cs
+++ Runtime/Services/ScreenService.cs
@@ -8,10 +8,16 @@ namespace Bumashuta
         [SerializeField] private FullScreenMode _mode;
         [SerializeField] private int _width = 640;
         [SerializeField] private int _height = 480;
+        [SerializeField] private bool _autoDetectResolution = true;
 
         protected override void AwakeSingleton()
         {
             base.AwakeSingleton();
+            if (_autoDetectResolution)
+            {
+                _width = Screen.currentResolution.width;
+                _height = Screen.currentResolution.height;
+            }
             Screen.SetResolution(_width, _height, _mode);
         }
     }
