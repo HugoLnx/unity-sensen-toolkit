3710ecc1f82360b3a46436e9a59904a002477683
hugolnx <hugolnx@gmail.com>
2025-06-07 15:01:30 -0300

    New RadioButton components

---
+++ Runtime/UI/RadioButton.cs
@@ -0,0 +1,81 @@
+using System;
+using EasyButtons;
+using MyBox;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SensenToolkit
+{
+    public class RadioButton : MonoBehaviour
+    {
+        [SerializeField] private bool _shouldDeactivateOnClick = false;
+        [Header("Inactive Colors")]
+        [SerializeField] private ColorBlock _inactiveColors;
+        [Header("Active Colors")]
+        [SerializeField] private ColorBlock _activeColors;
+        [SerializeField, AutoProperty] private Button _btn;
+
+        public bool IsActive { get; private set; }
+        public bool ShouldDeactivateOnClick
+        {
+            get => _shouldDeactivateOnClick;
+            set => _shouldDeactivateOnClick = value;
+        }
+        public event Action<RadioButton, bool> OnStateChanged;
+
+        private void OnEnable()
+        {
+            _btn.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            _btn.onClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            if (IsActive)
+            {
+                if (_shouldDeactivateOnClick) SwitchTo(false);
+                else return;
+            }
+            else
+            {
+                SwitchTo(true);
+            }
+        }
+
+        public void SwitchTo(bool turnOn, bool callbacks = true)
+        {
+            bool hasChanged = IsActive != turnOn;
+            IsActive = turnOn;
+            RefreshButtonState();
+            if (callbacks && hasChanged) OnStateChanged?.Invoke(this, IsActive);
+        }
+
+        public void Reset()
+        {
+            SwitchTo(false, callbacks: false);
+            RefreshButtonState();
+        }
+
+        private void RefreshButtonState()
+        {
+            if (IsActive) StyleAsActive();
+            else StyleAsInactive();
+        }
+
+        [Button]
+        private void StyleAsActive()
+        {
+            _btn.colors = _activeColors;
+        }
+
+        [Button]
+        private void StyleAsInactive()
+        {
+            _btn.colors = _inactiveColors;
+        }
+    }
+}
---
+++ Runtime/UI/RadioButtonGroup.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks.Triggers;
+using MyBox;
+using UnityEngine;
+
+namespace SensenToolkit
+{
+    public class RadioButtonGroup : MonoBehaviour
+    {
+        [SerializeField] private bool _canBeEmpty = false;
+        [SerializeField] private bool _canSelectMultiple = false;
+        [SerializeField] private List<int> _defaultActiveIndexes = new() { 0 };
+        [SerializeField, AutoProperty(AutoPropertyMode.Children)]
+        private RadioButton[] _radioButtons;
+        private List<int> _activeIndexes = new();
+        private List<RadioButton> _activeButtons = new();
+
+        public IReadOnlyList<int> ActiveIndexes => _activeIndexes;
+        public int ActiveIndex => _activeIndexes.Count == 0 ? -1 : _activeIndexes[0];
+        public RadioButton ActiveButton => ActiveIndex >= 0 ? _radioButtons[ActiveIndex] : null;
+        public IReadOnlyList<RadioButton> ActiveButtons => _activeButtons;
+        private bool _btnBlockOperation = false;
+
+        public Action OnStateChanged = delegate { };
+
+        private void Start()
+        {
+            foreach (RadioButton btn in _radioButtons)
+            {
+                btn.Reset();
+            }
+            SwitchToDefaults();
+        }
+
+        private void OnEnable()
+        {
+            foreach (RadioButton button in _radioButtons)
+            {
+                button.OnStateChanged += EnforceOperation;
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (RadioButton button in _radioButtons)
+            {
+                button.OnStateChanged -= EnforceOperation;
+            }
+        }
+
+        public void SwitchToDefaults() => SwitchTo(-1);
+        public void SwitchTo(int index, bool turnOn = true)
+        {
+            if (index < 0 || index >= _radioButtons.Length)
+            {
+                index = -1;
+            }
+
+            EnforceOperation(index == -1 ? null : _radioButtons[index], turnOn);
+        }
+
+        private void EnforceOperation(RadioButton btn, bool turnOn)
+        {
+            if (_btnBlockOperation) return;
+
+            _btnBlockOperation = true;
+            SwitchButtonsToEnforceOperation(btn, turnOn);
+            _btnBlockOperation = false;
+
+            RefreshGroupState();
+            OnStateChanged?.Invoke();
+        }
+
+        private void SwitchButtonsToEnforceOperation(RadioButton btn, bool turnOn)
+        {
+            foreach (RadioButton otherBtn in _radioButtons)
+            {
+                otherBtn.ShouldDeactivateOnClick = _canSelectMultiple || _canBeEmpty;
+            }
+
+            bool turnOff = !turnOn;
+            if (btn == null)
+            {
+                if (_canBeEmpty) SwitchAllOff();
+                else SwitchToDefaultIndexes();
+                return;
+            }
+
+            int activeCount = 0;
+            foreach (RadioButton otherBtn in _radioButtons)
+            {
+                if (otherBtn.IsActive) activeCount++;
+            }
+
+            bool willBecomeEmpty = turnOff && (activeCount == 0 || (activeCount == 1 && btn.IsActive));
+            if (!_canBeEmpty && willBecomeEmpty)
+            {
+                SwitchToDefaultIndexes();
+                return;
+            }
+
+            if (!_canSelectMultiple && turnOn)
+            {
+                SwitchOnExclusively(btn);
+                return;
+            }
+
+            btn.SwitchTo(turnOn);
+        }
+
+        private void SwitchAllOff()
+        {
+            foreach (RadioButton otherBtn in _radioButtons)
+            {
+                otherBtn.SwitchTo(false);
+            }
+        }
+
+        private void SwitchToDefaultIndexes()
+        {
+            HashSet<int> defaultIndexes;
+            if (!_canBeEmpty && _defaultActiveIndexes.Count == 0)
+            {
+                defaultIndexes = new HashSet<int> { 0 };
+            }
+            else if (!_canSelectMultiple && _defaultActiveIndexes.Count > 1)
+            {
+                defaultIndexes = new HashSet<int> { _defaultActiveIndexes[0] };
+            }
+            else
+            {
+                defaultIndexes = new HashSet<int>(_defaultActiveIndexes);
+            }
+            foreach (int inx in defaultIndexes)
+            {
+                _radioButtons[inx].SwitchTo(defaultIndexes.Contains(inx));
+            }
+        }
+
+        private void SwitchOnExclusively(RadioButton btn)
+        {
+            foreach (RadioButton otherBtn in _radioButtons)
+            {
+                otherBtn.SwitchTo(btn == otherBtn);
+            }
+        }
+
+        private void RefreshGroupState()
+        {
+            _activeIndexes.Clear();
+            _activeButtons.Clear();
+            for (int i = 0; i < _radioButtons.Length; i++)
+            {
+                if (_radioButtons[i].IsActive)
+                {
+                    _activeIndexes.Add(i);
+                    _activeButtons.Add(_radioButtons[i]);
+                }
+            }
+        }
+    }
+}
