4473fa02e66d0a4e76d64871ee1c1e528d5a6002
hugolnx <hugolnx@gmail.com>
2025-08-14 14:22:18 -0300

    Adding Env.IsBoothBuild and Env.IsProductionBuild

--- Runtime/Environment/Env.cs
+++ Runtime/Environment/Env.cs
@@ -2,9 +2,13 @@ namespace SensenToolkit
 {
     public static class Env
     {
-        public static bool IsDemoBuild
+        public static bool IsDemoBuild => RawIsDemoBuild || IsBoothBuild;
+        public static bool IsDebugBuild => RawIsDebugBuild || IsEditor;
+        public static bool IsProductionBuild => !(IsDebugBuild || IsBoothBuild || IsTrailerBuild);
+
+        public static bool IsBoothBuild
         {
-#if SENSEN_DEMO_BUILD
+#if SENSEN_BOOTH_BUILD
             get => true;
 #else
             get => false;
@@ -20,7 +24,24 @@ namespace SensenToolkit
 #endif
         }
 
-        public static bool IsDebugBuild
+        public static bool IsEditor
+        {
+#if UNITY_EDITOR
+            get => true;
+#else
+            get => false;
+#endif
+        }
+        private static bool RawIsDemoBuild
+        {
+#if SENSEN_DEMO_BUILD
+            get => true;
+#else
+            get => false;
+#endif
+        }
+
+        private static bool RawIsDebugBuild
         {
 #if SENSEN_DEBUG_BUILD
             get => true;
