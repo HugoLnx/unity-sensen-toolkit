8e4ff11f188393587ed3a3a08c47e7fe7cbdd98b
hugolnx <hugolnx@gmail.com>
2026-02-13 09:43:19 -0300

    Improves button text styling implementation

--- Runtime/UI/Button/ButtonTextStyling.cs
+++ Runtime/UI/Button/ButtonTextStyling.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using EasyButtons;
 using MyBox;
@@ -12,6 +12,8 @@ namespace SensenToolkit
     {
         [SerializeField] private Color _normalColor = Color.white;
         [SerializeField] private Color _disabledColor = Color.red;
+        [SerializeField, AutoProperty(AutoPropertyMode.Parent, allowEmpty: true)]
+        private PanelFadable _parentPanel;
 
         [SerializeField, AutoProperty(AutoPropertyMode.Parent)]
         private Button _button;
@@ -19,8 +21,25 @@ namespace SensenToolkit
         [SerializeField, AutoProperty(AutoPropertyMode.Children)]
         private TMP_Text _text;
 
-        private IEnumerator Start()
+        private void OnEnable()
         {
+            if (_parentPanel != null)
+            {
+                _parentPanel.OnPrepareToShow += OnPrepareToShow;
+            }
+        }
+
+        private void Start()
+        {
+            StartCoroutine(DelayedRefreshStyling());
+        }
+
+        private void OnPrepareToShow(PanelFadable fadable)
+            => StartCoroutine(DelayedRefreshStyling());
+
+        private IEnumerator DelayedRefreshStyling()
+        {
+            RefreshStyling();
             yield return null;
             yield return null;
             RefreshStyling();
