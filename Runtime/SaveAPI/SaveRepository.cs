c3c8e9cd494f0f3834554f908b5cdf921b767cfd
hugolnx <hugolnx@gmail.com>
2026-02-14 17:16:40 -0300

    SaveRepository supports adding extension to the save file

--- Runtime/SaveAPI/SaveRepository.cs
+++ Runtime/SaveAPI/SaveRepository.cs
@@ -4,23 +4,31 @@ namespace SensenToolkit
 {
     public class SaveRepository<TData> where TData : ISaveRootData
     {
+        private const string DEFAULT_EXTENSION = ".save.dat";
+
         public string Key { get; private set; }
         private string _filenameKey;
         private string _subfolder;
 
-        private SaveRepository(string filenameKey, string subfolder = null)
+        private SaveRepository(
+            string filenameKey,
+            string subfolder = null,
+            string extension = DEFAULT_EXTENSION)
         {
             _filenameKey = filenameKey;
             _subfolder = subfolder;
-            Key = string.IsNullOrEmpty(subfolder) ? filenameKey : $"{subfolder}/{filenameKey}";
+            Key = string.IsNullOrEmpty(subfolder) ? filenameKey : $"{subfolder}/{filenameKey}{extension}";
         }
 
-        public static SaveRepository<TData> Create(string filenameKey, string subfolder = null)
+        public static SaveRepository<TData> Create(
+            string filenameKey,
+            string subfolder = null,
+            string extension = DEFAULT_EXTENSION)
         {
-            SimpleHashing hashing = new();
             return new SaveRepository<TData>(
-                filenameKey: hashing.SHA1Short(filenameKey),
-                subfolder: subfolder
+                filenameKey: SimpleHashing.Instance.SHA1Short(filenameKey),
+                subfolder: subfolder,
+                extension: extension
             );
         }
     }
