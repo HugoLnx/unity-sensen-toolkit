c0fdb354bed93ca5a4f0c56b5ef4990b27ef0419
hugolnx <hugolnx@gmail.com>
2026-03-20 09:53:00 -0300

    Utility mono to deactivate/activate a game object following a BoolValueSO

---
+++ Runtime/Utils/ActivateWhenBooleanValue.cs
@@ -0,0 +1,29 @@
+﻿using MyBox;
+using UnityEngine;
+
+namespace SensenToolkit
+{
+    public class ActivateWhenBooleanValue : MonoBehaviour
+    {
+        [SerializeField, MustBeAssigned] private BoolValueSO _valueSo;
+        [SerializeField] private bool _invert;
+
+        private void Awake()
+        {
+            _valueSo.OnValueChanged += OnValueChanged;
+            RefreshActiveState();
+        }
+
+        private void OnDestroy()
+        {
+            _valueSo.OnValueChanged -= OnValueChanged;
+        }
+
+        private void OnValueChanged(BoolValueSO _) => RefreshActiveState();
+
+        private void RefreshActiveState()
+        {
+            gameObject.SetActive(_invert ? !_valueSo.Value : _valueSo.Value);
+        }
+    }
+}
